eb927daaa7ffa7331bc027a233f47efce5ec4a0b
Greg Kluska <kluskagreg@gmail.com>
2021-05-25 23:51:20 +0100

    Add kotlin kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = Apps.compileSdk
+    buildToolsVersion = Apps.buildToolsVersion
+
+    defaultConfig {
+        applicationId = "com.gregkluska.quizapp"
+        minSdk = Apps.minSdk
+        targetSdk = Apps.targetSdk
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-dev"
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Libs.kotlin)
+    implementation(Libs.appcompat)
+    testImplementation(TestLibs.junit)
+}
